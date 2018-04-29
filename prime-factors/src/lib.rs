48bf771f0f937455db1fa33b9f803db223a0ea4e
Fredrik <fredrik.christenson@gmail.com>
2018-04-29 22:05:44 +0200

    prime factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,29 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut results = vec![];
+
+    if n <= 1 {
+        return results;
+    }
+
+    for number in 2..n + 1 {
+        if is_prime(number) && n % number == 0 {
+            results.push(number);
+            let remainder = n / number;
+            let mut factors = factors(remainder);
+            results.append(&mut factors);
+            break;
+        }
+    }
+
+    return results;
+}
+
+fn is_prime(n: u64) -> bool {
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    return n != 1;
 }
